7d6ce677c6c30b6e66aba94c29527571c0183e6f
mochi-sann <moyurusuto@gmail.com>
2021-03-19 23:26:55 +0900

    Footerを削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
+
 import Body from 'src/component/body'
 export const Home = (): JSX.Element => (
   <div className="container">
@@ -12,17 +12,6 @@ export const Home = (): JSX.Element => (
       <h1>各都道府県ごとの人口推移</h1>
       <Body />
     </main>
-
-    <footer>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by{' '}
-        <Image src="/vercel.svg" alt="Vercel Logo" height={'32'} width={'64'} />
-      </a>
-    </footer>
 
     <style jsx>{`
       .container {
@@ -40,25 +29,6 @@ export const Home = (): JSX.Element => (
         flex: 1;
         display: flex;
         flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
         justify-content: center;
         align-items: center;
       }
